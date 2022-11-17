080952d803ccfa6fd0e29c52a6ff854ddf7169b9
safwankajas <safwankajas02@gmail.com>
2022-11-18 00:28:21 +0530

    demo3

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,9 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        'spin-slow': 'spin 40s linear infinite',
+        'spin-slow': 'spin 40s linear infinite'
       }
+      ,
     },
   },
   plugins: [],
